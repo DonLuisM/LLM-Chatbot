baff4c1ba8fddd7fb3c78efb102f9c8979510575
DonLuisM <lfmb03@outlook.com>
2025-05-18 19:00:52 -0500

    feat(Updates): Updating captions st

--- src/app.py
+++ src/app.py
@@ -150,8 +150,9 @@ file_type=["jpg", "jpeg", "png"],)
 if user_input is None:
     st.caption("""1st Q: Cómo debería limpiar el módulo base del equipo? 
                \n\n 2nd Q: Que es el modo cortical? 
-               \n\n 3rd Q: Quien pertenecía al Reform-Club?
-               \n\n 4th Q: Quien es el autor del libro?""")
+               \n\n 3rd Q: Qué es TEMGQ
+               \n\n 4th Q: Quien es el autor del libro?
+               \n\n 5th Q: Quien pertenecía al Reform-Club?""")
 
 if st.session_state.pdf_vector and st.session_state.pdf_loaded:
     if user_input and user_input.text:
